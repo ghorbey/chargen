c8c37ca51c3cb5a6a3b3466d54bc73b348959d02
GhorghorBey <s.paquay@gmail.com>
2021-12-30 16:37:35 +0100

    Add prod server if production environment.

--- common/pool.js
+++ common/pool.js
@@ -1,12 +1,16 @@
 const { Pool } = require("pg");
 
-const credentials = {
+let credentials = {
     user: "kufhjuotoojmev",
-    host: "localhost"/*"ec2-52-31-201-170.eu-west-1.compute.amazonaws.com"*/,
+    host: "localhost",
     database: "d743i8seb04k2b",
     password: "ea95488de9cb28657b7cda1bf417d9e8d1b73302b84d5575ae44e66281bcfb06",
     port: 5432
 };
+
+if (process.env.NODE_ENV === 'production') {
+    credentials.host = "ec2-52-31-201-170.eu-west-1.compute.amazonaws.com";
+}
 
 const pool = new Pool(credentials);
 
