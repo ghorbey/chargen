5539bd225fd325a8625fbb15c22e1369edd4dbff
GhorghorBey <s.paquay@gmail.com>
2021-12-31 13:40:43 +0100

    Add sql.

---
+++ sql/all_tables_content.sql
@@ -0,0 +1,18 @@
+INSERT INTO careers VALUES ();
+
+INSERT INTO careers_skills VALUES ();
+
+INSERT INTO countries VALUES ();
+
+INSERT INTO races VALUES ();
+
+INSERT INTO religions VALUES ();
+
+INSERT INTO skills VALUES ();
+
+INSERT INTO vocations VALUES ();
+
+INSERT INTO users (user_firstname, user_lastname, phone_number, email, user_password, is_admin) VALUES ('Stéphane', 'Paquay', '0495/77.90.18', 's.paquay@gmail.com', 'e035ef866c1b02be27273d691b4e3b27', true);
+
+/*mdpdefred*/
+INSERT INTO users (user_firstname, user_lastname, phone_number, email, user_password, is_admin) VALUES ('Frédéric', 'Pire', '0484/24.41.20', 'wodin666@hotmail.com', '1157f43ff1a618bdee4b676a9974fabd', true);
