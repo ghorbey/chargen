5222ec7d02a2378bd2fe4ec57fbc1c18b0e8a3f2
GhorghorBey <s.paquay@gmail.com>
2022-01-13 00:53:57 +0100

    Fix player character display.

--- client/src/pages/CharacterUserPage.js
+++ client/src/pages/CharacterUserPage.js
@@ -6,8 +6,12 @@ import CharacterService from '../services/Character.service';
 import { Error, Loading, Character } from '../components';
 import { getCurrentUser } from '../common';
 
-function createNewCharacter(userId) {
-    return { id: 0, user_id: +userId, character_name: '', character_type: 'pj', character_number: '000', fate_points: 2, country_id: 1, race_id: 1, religion_id: 1, vocation_id: 1, current_xp: 0, total_xp: 0, public_legend: '', background: '' };
+function createNewCharacter(id, userId) {
+    if (+id === 0) {
+        return { id: 0, user_id: +userId, character_name: '', character_type: 'pj', character_number: '000', fate_points: 2, country_id: 1, race_id: 1, religion_id: 1, vocation_id: 1, current_xp: 0, total_xp: 0, public_legend: '', background: '', careers_history: [] };
+    } else {
+        return undefined;
+    }
 }
 
 export default function CharacterUserPage(props) {
